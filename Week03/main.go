53334bd7c57d44e9dc8aa6055976f7380603af7a
laoniu12138 <851917050@qq.com>
2020-12-09 23:36:45 +0800

    add week03 homework

--- Week03/main.go
+++ Week03/main.go
@@ -1,6 +1,3 @@
-作业：基于 errgroup 实现一个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够一个退出，全部注销退出。
-
-```go
 package main
 
 import (
@@ -71,5 +68,3 @@ func main() {
 	}
 	fmt.Println(ctx.Err())
 }
-```
-
